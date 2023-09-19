bd023ae275efc3c2f233f8c336b1f12de464d664
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-09-19 20:30:25 -0300

    Criado modules para cada entidade
    Co-authored-by: Artur Santos <artursanntos@users.noreply.github.com>

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaService } from './database/prisma.service';
+import { ColaboradorModule } from './modules/colaborador/colaborador.module';
+import { ColaboradorIndicadorModule } from './modules/colaborador-indicador/colaborador-indicador.module';
+import { GestorModule } from './modules/gestor/gestor.module';
+import { IndicadorModule } from './modules/indicador/indicador.module';
+import { NotaMensalModule } from './modules/nota-mensal/nota-mensal.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ColaboradorModule,
+    ColaboradorIndicadorModule,
+    GestorModule,
+    IndicadorModule,
+    NotaMensalModule,
+  ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
