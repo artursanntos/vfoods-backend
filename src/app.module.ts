c0a32cb19c61e4aba365cfd635a23894cb89989b
Nathalia Barbosa <nathaliaferdab@gmail.com>
2023-09-19 19:15:20 -0300

    rename prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './database/prisma.servidor';
+import { PrismaService } from './database/prisma.service';
 
 @Module({
   imports: [],
